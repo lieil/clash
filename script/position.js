13db13ac2257839326ce497362415b8202249887
Elena Frolova <elicfr@mail.ru>
2014-07-29 00:51:26 +0400

    created layout with selection of army items. Start sme js func

---
+++ script/position.js
@@ -0,0 +1,12 @@
+function selectItem(){
+	showItemBlock();
+	addItemInSet();
+}
+
+function showItemBlock(id){
+	var sd = "s" + number(id);
+	alert(sd);
+//	var elem = document.getElementById(sd);
+}
+
+ 
