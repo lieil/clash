53d64ccf44d2f7ae6e7c5b270885cb7b31c32ba2
Elena Frolova <elicfr@mail.ru>
2014-08-13 01:07:10 +0400

    все сломалось в функции обнуления значений

--- www/script/position.js
+++ www/script/position.js
@@ -1,33 +1,31 @@
-var set;
-var maxlvl;
+var set = [];
+var maxlvl = [];
 var lvl = [3,4,3,4,4,4,4,2,2,0,1,1,0,0,0,3,4,3,0,0];
 // jQuery 
 
+
 $(document).ready(function(){
 //Обнуляется сумма
-SUM = 0;
-$.get("baza.php",  function(data){
-	$("#resultCost").html(data);
-	set = $.parseJSON(data);
-	alert(set.length);
-/*	for(i=0, i<lvl.length, i++){
-		if(set[k].cost){
-			maxlvl[i] = (set[k].cost.length);
-		} else if (set[k].costd){
-			maxlvl[i] = (set[k].costd.length);;
-		} else {
-			alert("Ой, уровень какой-то непонятный...");
+	$.get("baza.php",  function(data){
+		$("#resultCost").html(data);
+		set = $.parseJSON(data);
+		for(var i=0; i<lvl.length; i++){
+				if(set[i].cost){
+					maxlvl[i] = set[i].cost.length;
+				} else if (set[i].costd){
+					maxlvl[i] = set[i].costd.length;
+				} else {
+					alert("Ой, уровень какой-то непонятный...");
+				}
 		}
-		alert(maxlvl[i]);
-		alert(lvl[i]);
-	}*/
-});
+		alert(maxlvl);
+	});
 	//устанавливается значение аттрибутов для полей ввода
 	$(".num").attr({"value":"0", "type":"text", "size":"7", "step":"1"});
-	$(".num").val(0);
+	resetNum();
 	$(".num").change(function(){
 		if (!isNumeric($(this).val())){ 
-			alert("Введите число воинов данного типа");
+			alert("Введите число воинов данного типа. Цифрами, пожалуйста!");
 			$(this).val(0);
 		}
 		countArmy();
@@ -37,7 +35,6 @@ $.get("baza.php",  function(data){
 	// и выполняются другие скрытые действия, удаляющие юнит из активного набора
     $(".cross").click(function(){
         $(this.parentNode).animate({ opacity: "hide" }, "slow", hideItemBlock(this.parentNode));
-//		$(this).val(0);
 		countArmy();
     });
 	
@@ -49,41 +46,49 @@ $.get("baza.php",  function(data){
 
 });
 
-function countArmy(){
-	var result = 0;
-	var resultd = 0;
-	var space = 0;
-	var k,l;
-	$(".num").each(function(){
-		k = parseInt(this.parentNode.id.substr(1,2))-1;
-		l = lvl[k]-1;
-		if(l >= 0){
-			if(set[k].cost){
-				result += (set[k].cost[l])*($(this).val());
-			} else if (set[k].costd){
-				resultd += (set[k].costd[l])*($(this).val());
-			} else {
-				alert("Ой, цены какие-то странные...");
+	function countArmy(){
+		var result = 0;
+		var resultd = 0;
+		var space = 0;
+		var k,l;
+		$(".num").each(function(){
+			k = parseInt(this.parentNode.id.substr(1,2))-1;
+//			alert("Считаем цену юнита №" + k);
+			l = lvl[k]-1;
+			if(l >= 0){
+				if(set[k].cost !== undefined && set[k].cost != null){
+					result += (set[k].cost[l])*($(this).val());
+				} else if (set[k].costd){
+					resultd += (set[k].costd[l])*($(this).val());
+				} else {
+					alert("Ой, цены какие-то странные...");
+				}
 			}
-		}
-		space += (set[k].space)*($(this).val());
-	});
-		
-	$("#resultCost").html("<span>Цена войска: </span> " + result + " <img src='./img/el-drop.png'/><br/><span>Цена темного войска: </span> " + resultd + " <img src='./img/dark-drop.png'/>");
-	$("#resultSpace").html("<span>Занимает места: </span> " + space);
+			space += (set[k].space)*($(this).val());
+		});
+			
+		$("#resultCost").html("<span>Цена войска: </span> " + result + " <img src='./img/el-drop.png'/><br/><span>Цена темного войска: </span> " + resultd + " <img src='./img/dark-drop.png'/>");
+		$("#resultSpace").html("<span>Занимает места: </span> " + space);
+	}
 	
-}
+	
+	function showItemBlock(elem){
+		var key = "s" + elem.id.substr(1,2);
+		var elem = document.getElementById(key);
+		elem.className = elem.className.replace(" closed", " ") + ' open';
+	}
 
-function showItemBlock(elem){
-	var key = "s" + elem.id.substr(1,2);
-	var elem = document.getElementById(key);
-	elem.className = elem.className.replace(" closed", " ") + ' open';
-}
+	function hideItemBlock(elem){
+		elem.className = elem.className.replace(" open", " ") + " closed";
+		$(elem).children(".num").val(0);
+	}
 
-function hideItemBlock(elem){
-	elem.className = elem.className.replace(" open", " ") + " closed";
-	$(elem).children(".num").val(0);
-}
+	function resetNum(){
+		$(".num").each(function(){
+			this.value = 0;
+		});
+		countArmy();
+	}
 
 // вспомогательные функции.
 // проверка на число
